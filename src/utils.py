75047a5c36a5d7483471346799d08d7b6477d4ee
ChethanDL <Chethan.22ai010@sode-edu.in>
2025-09-01 22:15:11 +0530

    wrote the prg for data transformation

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+from src.logger import logging
+from src.exceptions import CustomException
+import dill
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as f:
+            dill.dump(obj, f)
+    except Exception as e:
+        logging.error(f"Error occurred while saving object: {e}")
+        raise CustomException(e, sys)
